d707478734d5208d9b806057a75f67a3ab482cda
Max_Lekomscev <lekomscevmaksim@gmail.com>
2025-02-19 19:03:59 +0400

    Commit 66 (improved image_initialization.py for to good add folder with images in Nuitka, create the file with suitable nuitka-command)

--- functions/image_initialization.py
+++ functions/image_initialization.py
@@ -1,10 +1,21 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
+import importlib.resources
+from io import BytesIO
 
 
 # App (First display)
 def get_first_display_image() -> ctk.CTkImage:
-    first_d = Image.open("image/first_display.png")
+    try:
+        first_d = Image.open("images/first_display.png")
+    except FileNotFoundError:
+        # Opening the resource as binary data
+        with importlib.resources.open_binary('images', 'first_display.png') as resource_file:
+            img_data = resource_file.read()
+
+        # Loading an image from bytes
+        first_d = Image.open(BytesIO(img_data))
+
     first_display_image = ctk.CTkImage(first_d, size=(914, 333))
 
     return first_display_image
@@ -12,7 +23,16 @@ def get_first_display_image() -> ctk.CTkImage:
 
 # Task
 def get_button_save_image() -> ctk.CTkImage:
-    button_save = Image.open("image/save.png")
+    try:
+        button_save = Image.open("images/save.png")
+    except FileNotFoundError:
+        # Opening the resource as binary data
+        with importlib.resources.open_binary('images', 'save.png') as resource_file:
+            img_data = resource_file.read()
+
+        # Loading an image from bytes
+        button_save = Image.open(BytesIO(img_data))
+
     button_save_image = ctk.CTkImage(button_save, size=(59, 59))
 
     return button_save_image
@@ -20,15 +40,35 @@ def get_button_save_image() -> ctk.CTkImage:
 
 # AllResults
 def get_notebook_star_image() -> ImageTk.PhotoImage:
-    notebook_star = Image.open("image/notebook_star.png")
-    notebook_star.thumbnail(size=(29, 28))
+    try:
+        notebook_star = Image.open("images/notebook_star.png")
+        notebook_star.thumbnail(size=(29, 28))
+    except FileNotFoundError:
+        # Opening the resource as binary data
+        with importlib.resources.open_binary('images', 'notebook_star.png') as resource_file:
+            img_data = resource_file.read()
+
+        # Loading an image from bytes
+        notebook_star = Image.open(BytesIO(img_data))
+        notebook_star.thumbnail(size=(29, 28))
+
     notebook_star_image = ImageTk.PhotoImage(notebook_star)
 
     return notebook_star_image
 
 def get_notebook_trophy_image() -> ImageTk.PhotoImage:
-    notebook_trophy = Image.open("image/notebook_trophy.png")
-    notebook_trophy.thumbnail(size=(24, 28))
+    try:
+        notebook_trophy = Image.open("images/notebook_trophy.png")
+        notebook_trophy.thumbnail(size=(24, 28))
+    except FileNotFoundError:
+        # Opening the resource as binary data
+        with importlib.resources.open_binary('images', 'notebook_trophy.png') as resource_file:
+            img_data = resource_file.read()
+
+        # Loading an image from bytes
+        notebook_trophy = Image.open(BytesIO(img_data))
+        notebook_trophy.thumbnail(size=(24, 28))
+
     notebook_trophy_image = ImageTk.PhotoImage(notebook_trophy)
 
     return notebook_trophy_image
